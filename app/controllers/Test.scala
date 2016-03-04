ba8a0cba23156cd5761e20b0dcd6732ae3510329
myeong <myeong@jessie.2uxr.com>
2016-03-05 00:32:59 +0900

    form case class added in

--- app/controllers/Test.scala
+++ app/controllers/Test.scala
@@ -5,6 +5,17 @@ import play.api.mvc._
 import javax.inject.Inject
 import play.api.i18n._
 
+import play.api.data._
+import play.api.data.Forms._
+
+case class UserData(username: String)
+
+val userForm = Form(
+	mapping(
+		"username" -> text,
+	)(UserData.apply)(UserData.unapply)
+)
+
 class Test @Inject() (val messagesApi: MessagesApi)
   extends Controller with I18nSupport {
 
